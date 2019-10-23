3d8aef9a32a067375fd566a6da60fbddcbea6aec
jimmyang <jimmy.yang@iisigroup.com>
2019-10-23 15:15:21 +0800

    first version support change one layout wiht fake shimmer layout

--- app/src/main/java/com/koca/rlistviewdemo/ViewDemoActivity.java
+++ app/src/main/java/com/koca/rlistviewdemo/ViewDemoActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.koca.shimmer.ShimmerView;
 
@@ -14,6 +15,7 @@ public class ViewDemoActivity extends AppCompatActivity implements SwipeRefreshL
 
     private ShimmerView skeletonScreen;
     private SwipeRefreshLayout refresh_layout;
+    private Button bt;
 
     Handler mHandler = new Handler();
 
@@ -22,8 +24,23 @@ public class ViewDemoActivity extends AppCompatActivity implements SwipeRefreshL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_demo);
 
-        refresh_layout = findViewById(R.id.reflash_layout);
-        refresh_layout.setOnRefreshListener(this);
+        /*refresh_layout = findViewById(R.id.reflash_layout);
+        refresh_layout.setOnRefreshListener(this);*/
+
+        bt = findViewById(R.id.refresh_bt);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skeletonScreen.show();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        skeletonScreen.hide();
+                    }
+                },5000);
+            }
+        });
 
         View rootView = findViewById(R.id.rootView);
         skeletonScreen = new ShimmerView(this , rootView);
